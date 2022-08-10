1d0d3b156800f47db81d0b84c9870e52b06379a1
keynslug <encube.ul@gmail.com>
2022-08-10 08:36:21 +0000

    Release valitydev/swag-payments@1e3855da753b630784362d0983283d2d630af8a4

--- src/swag_client_tokens_api.erl
+++ src/swag_client_tokens_api.erl
@@ -77,8 +77,5 @@ get_response_spec('create_payment_resource', 400) ->
 get_response_spec('create_payment_resource', 401) ->
     undefined;
 
-get_response_spec('create_payment_resource', 409) ->
-    {'ExternalIDConflictError', 'ExternalIDConflictError'};
-
 get_response_spec(_, _) ->
     error(invalid_response_code).
