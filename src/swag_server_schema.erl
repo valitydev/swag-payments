98ebeb4206e9c5773c3362e9617288010e829b02
ndiezel0 <ndiezel0@gmail.com>
2026-02-27 12:36:59 +0000

    Release valitydev/swag-payments@8a22ed107a2adedfbc6db5f21aac20b5de23e9e1

--- src/swag_server_schema.erl
+++ src/swag_server_schema.erl
@@ -144,6 +144,22 @@ get_raw() ->
         <<"summary">> => <<"Inspect user to use payment method">>,
         <<"operationId">> => <<"inspectUser">>,
         <<"parameters">> => [ #{
+          <<"name">> => <<"X-Request-ID">>,
+          <<"in">> => <<"header">>,
+          <<"description">> => <<"Unique identifier of the request to the system">>,
+          <<"required">> => true,
+          <<"type">> => <<"string">>,
+          <<"maxLength">> => 32,
+          <<"minLength">> => 1
+        }, #{
+          <<"name">> => <<"X-Request-Deadline">>,
+          <<"in">> => <<"header">>,
+          <<"description">> => <<"Maximum request processing time">>,
+          <<"required">> => false,
+          <<"type">> => <<"string">>,
+          <<"maxLength">> => 40,
+          <<"minLength">> => 1
+        }, #{
           <<"in">> => <<"body">>,
           <<"name">> => <<"UserInspectRequest">>,
           <<"description">> => <<"User inspection request">>,
