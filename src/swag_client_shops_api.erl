809f55d51a6647bfe67de8de58ad67479920c798
ndiezel0 <ndiezel0@gmail.com>
2026-03-23 01:55:18 +0000

    Release valitydev/swag-payments@3d1b9c167ac59a386ecbd78a45104df4e4e5784a

---
+++ src/swag_client_shops_api.erl
@@ -0,0 +1,309 @@
+%% -*- mode: erlang -*-
+-module(swag_client_shops_api).
+
+%% generated methods
+
+-export([activate_shop_for_party/2]).
+-export([activate_shop_for_party/3]).
+
+-export([get_shop_by_id_for_party/2]).
+-export([get_shop_by_id_for_party/3]).
+
+-export([get_shop_cash_limits_for_party/2]).
+-export([get_shop_cash_limits_for_party/3]).
+
+-export([get_shops_for_party/2]).
+-export([get_shops_for_party/3]).
+
+-export([suspend_shop_for_party/2]).
+-export([suspend_shop_for_party/3]).
+
+
+-spec activate_shop_for_party(Endpoint :: swag_client:endpoint(), Params :: map()) ->
+    {ok, Code :: integer(), RespHeaders :: list(), Response :: map()} |
+    {error, _Reason}.
+activate_shop_for_party(Endpoint, Params) ->
+    activate_shop_for_party(Endpoint, Params, []).
+
+-spec activate_shop_for_party(Endpoint :: swag_client:endpoint(), Params :: map(), Opts :: swag_client:transport_opts()) ->
+    {ok, Code :: integer(), RespHeaders :: list(), Response :: map()} |
+    {error, _Reason}.
+activate_shop_for_party(Endpoint, Params, Opts) ->
+    process_response(swag_client_procession:process_request(
+        put,
+        swag_client_utils:get_url(Endpoint, "/v2/processing/parties/:partyID/shops/:shopID/activate"),
+        Params,
+        get_request_spec(activate_shop_for_party),
+        Opts
+    ), activate_shop_for_party).
+
+-spec get_shop_by_id_for_party(Endpoint :: swag_client:endpoint(), Params :: map()) ->
+    {ok, Code :: integer(), RespHeaders :: list(), Response :: map()} |
+    {error, _Reason}.
+get_shop_by_id_for_party(Endpoint, Params) ->
+    get_shop_by_id_for_party(Endpoint, Params, []).
+
+-spec get_shop_by_id_for_party(Endpoint :: swag_client:endpoint(), Params :: map(), Opts :: swag_client:transport_opts()) ->
+    {ok, Code :: integer(), RespHeaders :: list(), Response :: map()} |
+    {error, _Reason}.
+get_shop_by_id_for_party(Endpoint, Params, Opts) ->
+    process_response(swag_client_procession:process_request(
+        get,
+        swag_client_utils:get_url(Endpoint, "/v2/processing/parties/:partyID/shops/:shopID"),
+        Params,
+        get_request_spec(get_shop_by_id_for_party),
+        Opts
+    ), get_shop_by_id_for_party).
+
+-spec get_shop_cash_limits_for_party(Endpoint :: swag_client:endpoint(), Params :: map()) ->
+    {ok, Code :: integer(), RespHeaders :: list(), Response :: map()} |
+    {error, _Reason}.
+get_shop_cash_limits_for_party(Endpoint, Params) ->
+    get_shop_cash_limits_for_party(Endpoint, Params, []).
+
+-spec get_shop_cash_limits_for_party(Endpoint :: swag_client:endpoint(), Params :: map(), Opts :: swag_client:transport_opts()) ->
+    {ok, Code :: integer(), RespHeaders :: list(), Response :: map()} |
+    {error, _Reason}.
+get_shop_cash_limits_for_party(Endpoint, Params, Opts) ->
+    process_response(swag_client_procession:process_request(
+        get,
+        swag_client_utils:get_url(Endpoint, "/v2/processing/parties/:partyID/shops/:shopID/cash-limits"),
+        Params,
+        get_request_spec(get_shop_cash_limits_for_party),
+        Opts
+    ), get_shop_cash_limits_for_party).
+
+-spec get_shops_for_party(Endpoint :: swag_client:endpoint(), Params :: map()) ->
+    {ok, Code :: integer(), RespHeaders :: list(), Response :: map()} |
+    {error, _Reason}.
+get_shops_for_party(Endpoint, Params) ->
+    get_shops_for_party(Endpoint, Params, []).
+
+-spec get_shops_for_party(Endpoint :: swag_client:endpoint(), Params :: map(), Opts :: swag_client:transport_opts()) ->
+    {ok, Code :: integer(), RespHeaders :: list(), Response :: map()} |
+    {error, _Reason}.
+get_shops_for_party(Endpoint, Params, Opts) ->
+    process_response(swag_client_procession:process_request(
+        get,
+        swag_client_utils:get_url(Endpoint, "/v2/processing/parties/:partyID/shops"),
+        Params,
+        get_request_spec(get_shops_for_party),
+        Opts
+    ), get_shops_for_party).
+
+-spec suspend_shop_for_party(Endpoint :: swag_client:endpoint(), Params :: map()) ->
+    {ok, Code :: integer(), RespHeaders :: list(), Response :: map()} |
+    {error, _Reason}.
+suspend_shop_for_party(Endpoint, Params) ->
+    suspend_shop_for_party(Endpoint, Params, []).
+
+-spec suspend_shop_for_party(Endpoint :: swag_client:endpoint(), Params :: map(), Opts :: swag_client:transport_opts()) ->
+    {ok, Code :: integer(), RespHeaders :: list(), Response :: map()} |
+    {error, _Reason}.
+suspend_shop_for_party(Endpoint, Params, Opts) ->
+    process_response(swag_client_procession:process_request(
+        put,
+        swag_client_utils:get_url(Endpoint, "/v2/processing/parties/:partyID/shops/:shopID/suspend"),
+        Params,
+        get_request_spec(suspend_shop_for_party),
+        Opts
+    ), suspend_shop_for_party).
+
+process_response({ok, Code, Headers, RespBody}, OperationID) ->
+    try swag_client_procession:process_response(
+        get_response_spec(OperationID, Code),
+        RespBody
+    ) of
+        {ok, Resp} ->
+            {ok, Code, Headers, Resp};
+        Error ->
+            Error
+    catch
+        error:invalid_response_code ->
+            {error, {invalid_response_code, Code}}
+    end;
+process_response(Error, _) ->
+    Error.
+
+
+-spec get_request_spec(OperationID :: swag_client:operation_id()) ->
+    Spec :: swag_client_procession:request_spec() | no_return().
+
+
+get_request_spec('activate_shop_for_party') ->
+    [
+        {'X-Request-ID', #{
+            source => header,
+            rules  => [{type, 'binary'}, {max_length, 32}, {min_length, 1}, true
+, {required, true}]
+        }},
+        {'shopID', #{
+            source => binding,
+            rules  => [{type, 'binary'}, {max_length, 40}, {min_length, 1}, true
+, {required, true}]
+        }},
+        {'partyID', #{
+            source => binding,
+            rules  => [{type, 'binary'}, true
+, {required, true}]
+        }},
+        {'X-Request-Deadline', #{
+            source => header,
+            rules  => [{type, 'binary'}, {max_length, 40}, {min_length, 1}, true
+, {required, false}]
+        }}
+    ];
+get_request_spec('get_shop_by_id_for_party') ->
+    [
+        {'X-Request-ID', #{
+            source => header,
+            rules  => [{type, 'binary'}, {max_length, 32}, {min_length, 1}, true
+, {required, true}]
+        }},
+        {'shopID', #{
+            source => binding,
+            rules  => [{type, 'binary'}, {max_length, 40}, {min_length, 1}, true
+, {required, true}]
+        }},
+        {'partyID', #{
+            source => binding,
+            rules  => [{type, 'binary'}, true
+, {required, true}]
+        }},
+        {'X-Request-Deadline', #{
+            source => header,
+            rules  => [{type, 'binary'}, {max_length, 40}, {min_length, 1}, true
+, {required, false}]
+        }}
+    ];
+get_request_spec('get_shop_cash_limits_for_party') ->
+    [
+        {'X-Request-ID', #{
+            source => header,
+            rules  => [{type, 'binary'}, {max_length, 32}, {min_length, 1}, true
+, {required, true}]
+        }},
+        {'shopID', #{
+            source => binding,
+            rules  => [{type, 'binary'}, {max_length, 40}, {min_length, 1}, true
+, {required, true}]
+        }},
+        {'partyID', #{
+            source => binding,
+            rules  => [{type, 'binary'}, true
+, {required, true}]
+        }},
+        {'X-Request-Deadline', #{
+            source => header,
+            rules  => [{type, 'binary'}, {max_length, 40}, {min_length, 1}, true
+, {required, false}]
+        }}
+    ];
+get_request_spec('get_shops_for_party') ->
+    [
+        {'X-Request-ID', #{
+            source => header,
+            rules  => [{type, 'binary'}, {max_length, 32}, {min_length, 1}, true
+, {required, true}]
+        }},
+        {'partyID', #{
+            source => binding,
+            rules  => [{type, 'binary'}, true
+, {required, true}]
+        }},
+        {'X-Request-Deadline', #{
+            source => header,
+            rules  => [{type, 'binary'}, {max_length, 40}, {min_length, 1}, true
+, {required, false}]
+        }}
+    ];
+get_request_spec('suspend_shop_for_party') ->
+    [
+        {'X-Request-ID', #{
+            source => header,
+            rules  => [{type, 'binary'}, {max_length, 32}, {min_length, 1}, true
+, {required, true}]
+        }},
+        {'shopID', #{
+            source => binding,
+            rules  => [{type, 'binary'}, {max_length, 40}, {min_length, 1}, true
+, {required, true}]
+        }},
+        {'partyID', #{
+            source => binding,
+            rules  => [{type, 'binary'}, true
+, {required, true}]
+        }},
+        {'X-Request-Deadline', #{
+            source => header,
+            rules  => [{type, 'binary'}, {max_length, 40}, {min_length, 1}, true
+, {required, false}]
+        }}
+    ].
+
+-spec get_response_spec(OperationID :: swag_client:operation_id(), Code :: swag_client_procession:code()) ->
+    Spec :: swag_client_procession:response_spec() | no_return().
+
+
+get_response_spec('activate_shop_for_party', 204) ->
+    undefined;
+
+get_response_spec('activate_shop_for_party', 400) ->
+    {'DefaultLogicError', 'DefaultLogicError'};
+
+get_response_spec('activate_shop_for_party', 401) ->
+    undefined;
+
+get_response_spec('activate_shop_for_party', 404) ->
+    {'GeneralError', 'GeneralError'};
+
+get_response_spec('get_shop_by_id_for_party', 200) ->
+    {'Shop', 'Shop'};
+
+get_response_spec('get_shop_by_id_for_party', 400) ->
+    {'DefaultLogicError', 'DefaultLogicError'};
+
+get_response_spec('get_shop_by_id_for_party', 401) ->
+    undefined;
+
+get_response_spec('get_shop_by_id_for_party', 404) ->
+    {'GeneralError', 'GeneralError'};
+
+get_response_spec('get_shop_cash_limits_for_party', 200) ->
+    {'list', 'inline_response_200_3'};
+
+get_response_spec('get_shop_cash_limits_for_party', 400) ->
+    {'DefaultLogicError', 'DefaultLogicError'};
+
+get_response_spec('get_shop_cash_limits_for_party', 401) ->
+    undefined;
+
+get_response_spec('get_shop_cash_limits_for_party', 404) ->
+    {'GeneralError', 'GeneralError'};
+
+get_response_spec('get_shops_for_party', 200) ->
+    {'list', 'Shop'};
+
+get_response_spec('get_shops_for_party', 400) ->
+    {'DefaultLogicError', 'DefaultLogicError'};
+
+get_response_spec('get_shops_for_party', 401) ->
+    undefined;
+
+get_response_spec('get_shops_for_party', 404) ->
+    {'GeneralError', 'GeneralError'};
+
+get_response_spec('suspend_shop_for_party', 204) ->
+    undefined;
+
+get_response_spec('suspend_shop_for_party', 400) ->
+    {'DefaultLogicError', 'DefaultLogicError'};
+
+get_response_spec('suspend_shop_for_party', 401) ->
+    undefined;
+
+get_response_spec('suspend_shop_for_party', 404) ->
+    {'GeneralError', 'GeneralError'};
+
+get_response_spec(_, _) ->
+    error(invalid_response_code).
---
+++ src/swag_client_tokens_api.erl
@@ -0,0 +1,81 @@
+%% -*- mode: erlang -*-
+-module(swag_client_tokens_api).
+
+%% generated methods
+
+-export([create_payment_resource/2]).
+-export([create_payment_resource/3]).
+
+
+-spec create_payment_resource(Endpoint :: swag_client:endpoint(), Params :: map()) ->
+    {ok, Code :: integer(), RespHeaders :: list(), Response :: map()} |
+    {error, _Reason}.
+create_payment_resource(Endpoint, Params) ->
+    create_payment_resource(Endpoint, Params, []).
+
+-spec create_payment_resource(Endpoint :: swag_client:endpoint(), Params :: map(), Opts :: swag_client:transport_opts()) ->
+    {ok, Code :: integer(), RespHeaders :: list(), Response :: map()} |
+    {error, _Reason}.
+create_payment_resource(Endpoint, Params, Opts) ->
+    process_response(swag_client_procession:process_request(
+        post,
+        swag_client_utils:get_url(Endpoint, "/v2/processing/payment-resources"),
+        Params,
+        get_request_spec(create_payment_resource),
+        Opts
+    ), create_payment_resource).
+
+process_response({ok, Code, Headers, RespBody}, OperationID) ->
+    try swag_client_procession:process_response(
+        get_response_spec(OperationID, Code),
+        RespBody
+    ) of
+        {ok, Resp} ->
+            {ok, Code, Headers, Resp};
+        Error ->
+            Error
+    catch
+        error:invalid_response_code ->
+            {error, {invalid_response_code, Code}}
+    end;
+process_response(Error, _) ->
+    Error.
+
+
+-spec get_request_spec(OperationID :: swag_client:operation_id()) ->
+    Spec :: swag_client_procession:request_spec() | no_return().
+
+
+get_request_spec('create_payment_resource') ->
+    [
+        {'X-Request-ID', #{
+            source => header,
+            rules  => [{type, 'binary'}, {max_length, 32}, {min_length, 1}, true
+, {required, true}]
+        }},
+        {'PaymentResourceParams', #{
+            source => body,
+            rules  => [schema, {required, true}]
+        }},
+        {'X-Request-Deadline', #{
+            source => header,
+            rules  => [{type, 'binary'}, {max_length, 40}, {min_length, 1}, true
+, {required, false}]
+        }}
+    ].
+
+-spec get_response_spec(OperationID :: swag_client:operation_id(), Code :: swag_client_procession:code()) ->
+    Spec :: swag_client_procession:response_spec() | no_return().
+
+
+get_response_spec('create_payment_resource', 201) ->
+    {'PaymentResourceResult', 'PaymentResourceResult'};
+
+get_response_spec('create_payment_resource', 400) ->
+    {'DefaultLogicError', 'DefaultLogicError'};
+
+get_response_spec('create_payment_resource', 401) ->
+    undefined;
+
+get_response_spec(_, _) ->
+    error(invalid_response_code).
